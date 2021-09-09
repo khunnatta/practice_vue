b3355f4db53712420a8bd689612571c51c48982f
khunnatta <61150735+khunnatta@users.noreply.github.com>
2021-09-10 02:44:16 +0700

    practic directives

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,80 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+
+    <h2> {{ subTitle }} : {{ age }} </h2>
+    <h2 v-if="age>30?true:false"> You are old </h2>
+
+    <button @click="addAge"> add age </button>
+    <button v-on:click="resetAge"> resetAge </button> <br>
+
+    <h3> Use function : <input @keyup="readAge"/> </h3>
+    <h3> Use V-model : <input v-model="age"/> </h3>
+    <h3> Use V-bind & V-on <input v-on:keyup="readAge" v-bind:value="age"> </h3>
+
+    <h3> use Form: 
+    <form @submit="addAge">
+      <input type="submit">
+    </form>
+    </h3>
+
+    <h3> Test v-for in array </h3>
+
+    <ul v-for="i in myArr">
+      <li> {{ i }} </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+
+    <h3> Select in object (value, key, index) </h3>
+    <ul v-for="(value, key, index) in myObj">
+      <li> {{value}}, {{key}}, {{index}} </li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+
+    
+
+    <!-- <img :src="myImg" alt=""> -->
+
+
+
+
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+
+  data () {
+    return {
+    msg: "Hello World",
+    subTitle: "age",
+    age: 25,
+    myImg: 'https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg',
+    myArr: ['cat', 'dog', 'rat', 'bat'],
+    myObj: {
+      x: 10,
+      y: 20,
+      z: 20
+    }
+    }
+  },
+  
+  methods: {
+    addAge: function() {
+      this.age++;
+    },
+    resetAge: function() {
+      this.age = 25;
+    },
+    readAge: function(input) {
+      input.preventDefault();
+      this.age = input.target.value;
+    }
+  },
+
   props: {
-    msg: String
+    prop: String
   }
 }
 </script>
@@ -55,5 +94,13 @@ li {
 }
 a {
   color: #42b983;
+}
+
+button{
+  margin: 10px;
+}
+
+img{
+  margin: 10px 0 0 0;
 }
 </style>
